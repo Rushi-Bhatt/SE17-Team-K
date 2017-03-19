5c6ea9d3bfcdc82904f6bb2a2d3b304b61b5eab4
Rushi-Bhatt <rbhatt@ncsu.edu>
2017-03-18 22:37:49 -0400

    SliderImplementation

--- FeedbackApp/app/controllers/prof_ratings_controller.rb
+++ FeedbackApp/app/controllers/prof_ratings_controller.rb
@@ -8,17 +8,18 @@ class ProfRatingsController < ApplicationController
   end
 
   def create
+    logger.info params
     @prof_rating_id = ProfRating.where(:professor_id=>@@prof_id.to_i,
-                                       :doubt_solving => params[:doubt_solving].to_i,
-                                       :knowledge => params[:knowledge].to_i,
-                                       :course_material => params[:course_material].to_i,
-                                       :fluency => params[:fluency].to_i).first
+                                       :doubt_solving => params[:prof_rating][:doubt_solving].to_i,
+                                       :knowledge => params[:prof_rating][:knowledge].to_i,
+                                       :course_material => params[:prof_rating][:course_material].to_i,
+                                       :fluency => params[:prof_rating][:fluency].to_i).first
     if @prof_rating_id.nil? then
       @prof_rating_id = ProfRating.new(:professor_id=>@@prof_id.to_i,
-                                       :doubt_solving => params[:doubt_solving].to_i,
-                                       :knowledge => params[:knowledge].to_i,
-                                       :course_material => params[:course_material].to_i,
-                                       :fluency => params[:fluency].to_i)
+                                       :doubt_solving => params[:prof_rating][:doubt_solving].to_i,
+                                       :knowledge => params[:prof_rating][:knowledge].to_i,
+                                       :course_material => params[:prof_rating][:course_material].to_i,
+                                       :fluency => params[:prof_rating][:fluency].to_i)
 
       if @prof_rating_id.save
         #redirect to course feedback forrm with prof rating as a parameter
