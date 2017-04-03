cc9551105e096503006760328d31c9861f2287b5
popoosl <flypopoo@gmail.com>
2017-04-03 01:44:30 -0400

    modify search

--- FeedbackApp/app/controllers/courses_controller.rb
+++ FeedbackApp/app/controllers/courses_controller.rb
@@ -1,18 +1,7 @@
 class CoursesController < ApplicationController
 
   def search_course
-    number = course_params[:course_number]
-    prof = course_params[:professor_id]
-
-    if number.size==0&&prof.size==0
-      @courses = Course.all
-    elsif number.size!=0&&prof.size==0
-      @courses =Course.where(id: course_params[:course_number])
-    elsif number.size==0&&prof.size!=0
-      @courses =Course.where(professor_id: course_params[:professor_id])
-    else
-      @courses =Course.where(id: course_params[:course_number], professor_id: course_params[:professor_id])
-    end
+    @courses =Course.where(:course_number => course_params[:course_number], :professor_id => course_params[:id])
   end
 
   def show_course
@@ -35,5 +24,5 @@ end
   private
 
   def course_params
-    params.require(:course).permit(:course_number, :professor_id)
+    params.require(:course).permit(:course_number, :id)
   end
